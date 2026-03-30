ca1e7ec14b07d47cf9ea7158d74e6259f9a85ba7
Juan Ignacio Bonini <juanignaciobonini@hotmail.com>
2026-03-30 20:58:50 -0300

    Clase 1

--- clase1.py
+++ clase1.py
@@ -1,5 +1,34 @@
 # Microsoft Visual Studio Code (vscode)
 
+# Esto es una cadena de texto (string) (str)
 saludo = "Hola mundo!"
+saludo = "Hola"
+saludo = "chau"
+
+
+
 print(saludo)
 
+# Numero entero (integer) (int)
+resultado = 4
+
+# Numero de punto flotante (float) 
+resultado2 = 10.5
+
+"""Regla de nombre para variables:
+No pueden empezar con numeros
+123hola = 10 (esto no puede ser una variable)
+!"#$%%&&/(hola = 10 (No puede empezar con caracteres especiales)
+hola! = 10
+formato de variable = 10 (No se puede usar espacios)
+
+Si puede empezar con _
+_hola = 10 (Es una variable privada)
+
+formato_de_variable = "Este es el formato" snake case
+
+FormatoDeVariable = "Upper Camel Case" No se usa para variables
+formatoDeVariable = "Camel Case" No se usa para variables
+"""
+
+print(resultado)
